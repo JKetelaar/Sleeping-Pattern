ec50634bf873a011c8ef70c12c4638654931a3dc
JKetelaar <jeroen@ketelaar.me>
2016-09-30 12:19:53 +0200

    [FEATURE] Finished playing with TwitteR library

--- Twitter/app.R
+++ Twitter/app.R
@@ -11,22 +11,18 @@ library(RColorBrewer)
 api_key <- "TplDnL2Z1Fid7gCsHolTomxQT"
 api_secret <- "IgPFawGXN13LJ2iQj1rSKYekMwaOkT8tvV5H3wQ6ANDADDc0XI"
 access_token <- "237631092-NMov8Bzx069RmnQdypCU5K0mBSz0diCIoOAzRywE"
-access_token_secret <- "NWGeKvundNCbb2i2iaxTm2BzaZUNxAtL7UDl50Qyac1Wx"
-setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+access_token_secret <-
+  "NWGeKvundNCbb2i2iaxTm2BzaZUNxAtL7UDl50Qyac1Wx"
+setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
 
-user <- getUser("thom1459") #Set the username
-
-userFriends <- user$getFriends()
-userFollowers <- user$getFollowers()
-userNeighbors <- union(userFollowers, userFriends) #merge followers and friends
-
-userNeighbors.df = twListToDF(userNeighbors) #create the dataframe
-head(userNeighbors.df)
-
-slapen_tweets = searchTwitter("trusten", n = 200, lang="nl", since="2016-09-21")
-head(slapen_tweets)
-
-ecoli = searchTwitter("\"trusten\"", lang="nl", n = 200, since= '2016-09-28',until ='2016-09-29', geocode = '51.9,4.4,500mi')
+ecoli = searchTwitter(
+  "\"trusten\"",
+  lang = "nl",
+  n = 200,
+  since = '2016-09-28',
+  until = '2016-09-29',
+  geocode = '51.9,4.4,500mi'
+)
 
 ecoli2 = do.call(rbind, lapply(ecoli, function(x) {
   c(
@@ -41,5 +37,12 @@ ecoli2 = do.call(rbind, lapply(ecoli, function(x) {
 }))
 
 x <- as.data.frame(ecoli2)
-x
-plot(ecoli2$date)
+
+id <- as.character(x$id)
+date <- as.numeric(as.character(x$date))
+retweet <- as.integer(as.logical(x$retweet))
+name <- as.character(x$name)
+tweet <- as.character(x$tweet)
+
+# Convert timestamp into a date object
+# $date <- as.Date(as.POSIXct(timestamp, origin="1970-01-01"))
