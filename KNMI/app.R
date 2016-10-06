db61d2248640e3ccd23ae7c182a4eb95ce8ede6c
JKetelaar <jeroen@ketelaar.me>
2016-10-06 21:15:47 +0200

    [FEATURE] Added KNMI basics

---
+++ KNMI/app.R
@@ -0,0 +1,15 @@
+install.packages("devtools")
+
+library("devtools")
+install_github("Ram-N/weatherData")
+
+x <- getWeatherForDate("IZUIDHOL301", "2016-10-06", opt_detailed=TRUE, opt_all_columns=TRUE, station_type="ID")
+
+date <- x$Time
+source <- "IZUIDHOL301"
+wind <- x$WindSpeedKMH
+temp <- x$TemperatureC
+min_temp <- min(x$TemperatureC)
+max_temp <- max(x$TemperatureC)
+air_pressure <- x$PressurehPa
+rain <- x$dailyrainMM
