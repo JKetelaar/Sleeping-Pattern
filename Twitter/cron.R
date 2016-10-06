4d8777e7224b6fbf30005c6f4693804ea912eb04
JKetelaar <jeroen@ketelaar.me>
2016-10-06 22:17:13 +0200

    [CLEANUP] Cleaned code

--- Twitter/cron.R
+++ Twitter/cron.R
@@ -8,32 +8,32 @@ setup_twitter_oauth(
   config$twitter$api_key,
   config$twitter$api_secret,
   config$twitter$access_token,
-  config$twitter$access_token_secret)
+  config$twitter$access_token_secret
+)
 
 toDataFrame <- function(result, longitude, latitude) {
   do.call(rbind,
-    lapply(result,
-      function(x) {
-        long <- x$getLongitude()
-        if(is.null(long) || is.character(long) && length(long) == 0) {
-          long <- longitude
-        }
-        lat <- x$getLatitude()
-        if(is.null(lat) || is.character(lat) && length(lat) == 0) {
-          lat <- latitude
-        }
-        c(
-          id = x$getId(),
-          date = x$getCreated(),
-          longitude = long,
-          latitude = lat,
-          retweet = x$getIsRetweet(),
-          name = x$getScreenName(),
-          tweet = x$getText()
-        )
-      }
-    )
-  )
+          lapply(result,
+                 function(x) {
+                   long <- x$getLongitude()
+                   if (is.null(long) ||
+                       is.character(long) && length(long) == 0) {
+                     long <- longitude
+                   }
+                   lat <- x$getLatitude()
+                   if (is.null(lat) || is.character(lat) && length(lat) == 0) {
+                     lat <- latitude
+                   }
+                   c(
+                     id = x$getId(),
+                     date = x$getCreated(),
+                     longitude = long,
+                     latitude = lat,
+                     retweet = x$getIsRetweet(),
+                     name = x$getScreenName(),
+                     tweet = x$getText()
+                   )
+                 }))
 }
 
 getData <- function(country, region, term) {
@@ -43,21 +43,26 @@ getData <- function(country, region, term) {
     n = settings$results,
     since = Sys.Date() - 1,
     until = Sys.Date(),
-    geocode = paste(region$longitude, region$latitude, paste(region$radius, 'km', sep = ''), sep = ',')
+    geocode = paste(
+      region$longitude,
+      region$latitude,
+      paste(region$radius, 'km', sep = ''),
+      sep = ','
+    )
   )
   toDataFrame(result, region$longitude, region$latitude)
 }
 
 data <- NULL
 
-for(country in settings$countries) {
-  for(region in country$regions) {
-    for(term in country$terms) {
+for (country in settings$countries) {
+  for (region in country$regions) {
+    for (term in country$terms) {
       data <- rbind(data, getData(country, region, term))
     }
   }
 }
 #remove duplicates
 data <- as.data.frame(data)
-data <- data[duplicated(data$id),]
+data <- data[duplicated(data$id), ]
 #TODO: insert into db
