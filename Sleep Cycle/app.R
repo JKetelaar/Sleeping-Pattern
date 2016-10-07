8006a57b5013ebaddc8723177bc5dad0e6e7b169
JKetelaar <jeroen@ketelaar.me>
2016-10-07 15:13:57 +0200

    [FEATURE] Added database read

--- Sleep Cycle/app.R
+++ Sleep Cycle/app.R
@@ -1,7 +1,19 @@
 source("../common/common.R")
-loadPackages(c("lubridate", "plyr", "ggplot2", "rjson"))
+loadPackages(c("lubridate", "plyr", "ggplot2", "rjson", "RMySQL"))
 
 settings <- fromJSON(file = "../settings.json")
+config <- fromJSON(file = "../config.json")
+
+conn <- dbConnect(RMySQL::MySQL(),
+                  host = config$mysql$host,
+                  dbname = config$mysql$database,
+                  user = config$mysql$user,
+                  password = config$mysql$password)
+
+stringToMinutes <- function(x) {
+  x <- as.numeric(x)
+  x[1]*60 + x[2]
+}
 
 toDataFrame <- function(data, sep) {
   if (nrow(data) > 1) {
@@ -11,6 +23,14 @@ toDataFrame <- function(data, sep) {
       if (is.null(data[[tag]])) {
         data[[tag]] <- NA
       }
+      
+      if (tag == "time.in.bed"){
+        data[[tag]] <- sapply(strsplit(as.character(data[[tag]]), ":"), stringToMinutes)
+      }
+      
+      if (tag == "sleep.quality"){
+        data[[tag]] <- as.integer(gsub("%", "", as.character(data[[tag]])))
+      }
     }
     return(data)
   } else{
@@ -18,6 +38,16 @@ toDataFrame <- function(data, sep) {
   }
 }
 
-#sleep <-
-  #read.csv(file = "../data/sleep-cycle/sleepdata-thom.csv", header = TRUE, sep = ",")
-#result <- toDataFrame(sleep)
+removeDatabaseDuplicates <- function(data, username){
+  rs <- dbSendQuery(conn, sprintf("SELECT * FROM `sleep_cycle` WHERE `username` LIKE '%s'", dbEscapeStrings(conn, username)))
+  data <- fetch(rs, n=-1)
+  return(data)
+}
+
+# Examples
+# sleep <-read.csv(file = "../data/sleep-cycle/sleepdata-kwdoyle.csv", header = TRUE, sep = ",")
+# result <- toDataFrame(sleep)
+
+# Read folder
+# filenames <- list.files(path = "/Users/thomsuykerbuyk/github/sleepdata_test")
+# sleep <- do.call("rbind", lapply(filenames, read.csv, header = TRUE))
