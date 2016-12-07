33f76ac0efae9d2d402c4c0c7dc7b387baa6253b
Thomas Maurer <0876337@hr.nl>
2016-12-07 15:42:52 +0100

    Beginning combining data

---
+++ Sleep Aggregate/app.R
@@ -0,0 +1,16 @@
+TWITTER <- new.env()
+GTRENDS <- new.env()
+
+wd <- getwd()
+
+setwd('../Twitter/shiny')
+source('global.R', local = TWITTER)
+twitterData <- TWITTER$analyzeData(TWITTER$readData())
+
+setwd(wd)
+
+setwd('../GtrendsR/shiny')
+source('global.R', local = GTRENDS)
+gtrendsData <- GTRENDS$originalData
+
+setwd(wd)
