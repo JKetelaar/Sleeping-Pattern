fd07b232202d62f59122780362917ce1e8233e67
JKetelaar <jeroen@ketelaar.me>
2016-12-16 14:04:15 +0100

    [FEATURE] Added sleeps

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -4,19 +4,39 @@ source("../Sleep Aggregate/server.R")
 source("../Sleep Cycle/server.R")
 
 shinyServer(function(input, output) {
-  # Sleep Aggregate
-  output$regionC <- regionCAggregate(input, output)
-  output$sleep <- sleepAggregate(input, output)
-  output$totals <- totalsAggregate(input, output)
-  
-  # Your Sleep Cycle
-  observeEvent(input$do, {
-    output$outbed <- outbedSleepCycle(input, output)
-    output$contents <- contentsSleepCycle(input, output)
-  })
-  
-  # General Sleep Cycle
-  output$gTimeInBed <- generalTimeInBed(input, output)
-  output$gSleepDuration <- generalSleepDuration(input, output)
-  output$gTimeInBedMonth <- generalTimeInBedMonth(input, output)
+  withProgress(message = "Loading data",
+               detail = "Downloading dank memes",
+               value = 0,
+               {
+                 incProgress(0.1, detail = "Create observe events")
+                 # Your Sleep Cycle
+                 observeEvent(input$do, {
+                   output$outbed <- outbedSleepCycle(input, output)
+                   output$contents <-
+                     contentsSleepCycle(input, output)
+                 })
+                 
+                 # Sleep Aggregate
+                 incProgress(0.2, detail = "Plotting region aggregate")
+                 output$regionC <- regionCAggregate(input, output)
+                 
+                 incProgress(0.3, detail = "Plotting sleep aggregate")
+                 output$sleep <- sleepAggregate(input, output)
+                 
+                 incProgress(0.4, detail = "Plotting total aggregate")
+                 output$totals <- totalsAggregate(input, output)
+                 
+                 # General Sleep Cycle
+                 incProgress(0.6, detail = "Plotting time in bed")
+                 output$gTimeInBed <-
+                   generalTimeInBed(input, output)
+                 
+                 incProgress(0.8, detail = "Plotting sleep duration")
+                 output$gSleepDuration <-
+                   generalSleepDuration(input, output)
+                 
+                 incProgress(1, detail = "Plotting time in bed, monthly")
+                 output$gTimeInBedMonth <-
+                   generalTimeInBedMonth(input, output)
+               })
 })
