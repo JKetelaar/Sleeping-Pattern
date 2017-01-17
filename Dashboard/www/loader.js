9053959686e515deb660df3924e2633e5081bb9e
JKetelaar <jeroen@ketelaar.me>
2017-01-17 15:38:17 +0100

    [TASK] Hmmm not sure what I did....

--- Dashboard/www/loader.js
+++ Dashboard/www/loader.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var submitted = false;
   
-  var opts = {
+var opts = {
   lines: 13 // The number of lines to draw
 , length: 28 // The length of each line
 , width: 14 // The line thickness
@@ -13,7 +13,7 @@ $(document).ready(function(){
 , rotate: 0 // The rotation offset
 , direction: 1 // 1: clockwise, -1: counterclockwise
 , speed: 1 // Rounds per second
-, trail: 67 // Afterglow percentage
+, trail: 60 // Afterglow percentage
 , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
 , zIndex: 2e9 // The z-index (defaults to 2000000000)
 , className: 'spinner' // The CSS class to assign to the spinner
